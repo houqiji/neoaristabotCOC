320d2d0f466c670a012dd557f76d1a662929c1f0
nigrumdraco <houqiji@hotmail.com>
2022-05-22 23:59:34 +0800

    修改

--- nacocpy/coc_inv.py
+++ nacocpy/coc_inv.py
@@ -4,38 +4,36 @@ import math
 import random as rd
 
 
+class Attribute:
+    def __init__(self, STR=0, CON=0, SIZ=0, DEX=0, APP=0, INT=0, POW=0, EDU=0, DAM=0, PHY=0, MOV=0, lucky=0,
+                 health=0):
+        self.STR = STR
+        self.CON = CON
+        self.SIZ = SIZ
+        self.DEX = DEX
+        self.APP = APP
+        self.INT = INT
+        self.POW = POW
+        self.EDU = EDU
+        self.DAM = DAM
+        self.PHY = PHY
+        self.MOV = MOV
+        self.lucky = lucky
+        self.health = health
+
+
+class Occupation:
+    def __init__(self, a):
+        self.a = a
+
+
 class Investigator:
-    class Attribute:
-        def __init__(self, STR=0, CON=0, SIZ=0, DEX=0, APP=0, INT=0, POW=0, EDU=0, DAM=0, PHY=0, MOV=0, lucky=0,
-                     health=0):
-            self.STR = STR
-            self.CON = CON
-            self.SIZ = SIZ
-            self.DEX = DEX
-            self.APP = APP
-            self.INT = INT
-            self.POW = POW
-            self.EDU = EDU
-            self.DAM = DAM
-            self.PHY = PHY
-            self.MOV = MOV
-            self.lucky = lucky
-            self.health = health
-
-    class Occupation:
-        def __init__(self, a):
-            self.a = a
-
     def __init__(self, user_id, difficult, age, Attr=Attribute()):
         self.user_id = user_id
         self.Attr = Attr
         self.dice = cdi.CocDice(user_id)
         self.difficult = difficult
         self.age = age
-
-    def __get__(self, instance, owner):
-        if not 15 <= type(self.age) <= 90:
-            return cer.CocAgeError()
 
     @staticmethod
     def edu_enhance(attr, dice):
@@ -47,30 +45,25 @@ class Investigator:
 
     def roll_attribute(self):
         # change with difficult
-        if self.difficult == 0:
-            d = 1
-        elif self.difficult == 1:
-            d = 0.5
-        elif self.difficult == 2:
-            d = 0.2
+
         # STR       3D6*5
-        self.Attr.STR = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5)
+        self.Attr.STR = (self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5
         # CON       3D6*5
-        self.Attr.CON = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5)
+        self.Attr.CON = (self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5
         # SIZ       2D6+6*5
-        self.Attr.SIZ = math.floor((self.dice.d6() + self.dice.d6() + 6) * 5)
+        self.Attr.SIZ = (self.dice.d6() + self.dice.d6() + 6) * 5
         # DEX       3D6*5
-        self.Attr.DEX = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5)
+        self.Attr.DEX = (self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5
         # APP       3D6*5
-        self.Attr.APP = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5)
+        self.Attr.APP = (self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5
         # INT       2D6+6*5
-        self.Attr.INT = math.floor((self.dice.d6() + self.dice.d6() + 6) * 5)
+        self.Attr.INT = (self.dice.d6() + self.dice.d6() + 6) * 5
         # POW       3D6*5
-        self.Attr.POW = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5)
+        self.Attr.POW = (self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5
         # EDU       2D6+6*5
-        self.Attr.EDU = math.floor((self.dice.d6() + self.dice.d6() + 6) * 5)
+        self.Attr.EDU = (self.dice.d6() + self.dice.d6() + 6) * 5
         # lucky     3D6*5
-        self.Attr.lucky = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5)
+        self.Attr.lucky = (self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5
         # DAM and PHY
         if self.Attr.SIZ + self.Attr.STR <= 64:
             self.Attr.DAM = -2
@@ -124,59 +117,79 @@ class Investigator:
             self.Attr.STR -= cdi_list[0]
             self.Attr.SIZ -= cdi_list[1]
             self.Attr.EDU -= 5
-            lucky_2 = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5)
+            lucky_2 = (self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5
             lucky_1 = self.Attr.lucky
             self.Attr.lucky = max(lucky_1, lucky_2)
         elif self.age <= 29:
             self.Attr.EDU = self.edu_enhance(self.Attr.EDU, self.dice)
-            self.Attr.APP -= 5
         elif self.age <= 39:
             for i in range(2):
                 self.Attr.EDU = self.edu_enhance(self.Attr.EDU, self.dice)
-            self.Attr.APP -= 10
         elif self.age <= 49:
             for i in range(3):
                 self.Attr.EDU = self.edu_enhance(self.Attr.EDU, self.dice)
-            self.Attr.APP -= 15
             cdi_list = cdi.random_divide(3, 5)
             self.Attr.STR -= cdi_list[0]
             self.Attr.CON -= cdi_list[1]
             self.Attr.DEX -= cdi_list[2]
+            self.Attr.APP -= 5
         elif self.age <= 59:
             for i in range(4):
                 self.Attr.EDU = self.edu_enhance(self.Attr.EDU, self.dice)
-            self.Attr.APP -= 20
             cdi_list = cdi.random_divide(3, 10)
             self.Attr.STR -= cdi_list[0]
             self.Attr.CON -= cdi_list[1]
             self.Attr.DEX -= cdi_list[2]
+            self.Attr.APP -= 10
         elif self.age <= 69:
             for i in range(4):
                 self.Attr.EDU = self.edu_enhance(self.Attr.EDU, self.dice)
-            self.Attr.APP -= 25
             cdi_list = cdi.random_divide(3, 20)
             self.Attr.STR -= cdi_list[0]
             self.Attr.CON -= cdi_list[1]
             self.Attr.DEX -= cdi_list[2]
+            self.Attr.APP -= 15
         elif self.age <= 79:
             for i in range(4):
                 self.Attr.EDU = self.edu_enhance(self.Attr.EDU, self.dice)
-            self.Attr.APP -= 30
             cdi_list = cdi.random_divide(3, 30)
             self.Attr.STR -= cdi_list[0]
             self.Attr.CON -= cdi_list[1]
             self.Attr.DEX -= cdi_list[2]
+            self.Attr.APP -= 20
         elif self.age <= 89:
             for i in range(4):
                 self.Attr.EDU = self.edu_enhance(self.Attr.EDU, self.dice)
-            self.Attr.APP -= 35
             cdi_list = cdi.random_divide(3, 40)
             self.Attr.STR -= cdi_list[0]
             self.Attr.CON -= cdi_list[1]
             self.Attr.DEX -= cdi_list[2]
+            self.Attr.APP -= 25
+        # change difficult
+        d = 0
+        if self.difficult == 0:
+            d = 1
+        elif self.difficult == 1:
+            d = 0.5
+        elif self.difficult == 2:
+            d = 0.2
+        self.Attr.STR = math.floor(self.Attr.STR * d)
+        self.Attr.CON = math.floor(self.Attr.CON * d)
+        self.Attr.SIZ = math.floor(self.Attr.SIZ * d)
+        self.Attr.DEX = math.floor(self.Attr.DEX * d)
+        self.Attr.APP = math.floor(self.Attr.APP * d)
+        self.Attr.INT = math.floor(self.Attr.INT * d)
+        self.Attr.POW = math.floor(self.Attr.POW * d)
+        self.Attr.EDU = math.floor(self.Attr.EDU * d)
+        self.Attr.DAM = math.floor(self.Attr.DAM * d)
+        self.Attr.PHY = math.floor(self.Attr.PHY * d)
+        self.Attr.MOV = math.floor(self.Attr.MOV * d)
+        self.Attr.lucky = math.floor(self.Attr.lucky * d)
+
+    # def choose_occupation(self):
 
 
-inv = Investigator('10000000', 0, 100)
+inv = Investigator('10000000', 2, 100)
 inv.roll_attribute()
 print('STR', inv.Attr.STR)
 print('CON', inv.Attr.CON)
