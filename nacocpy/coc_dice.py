b9c06390088b0d904603872b63d6a34804792738
nigrumdraco <houqiji@hotmail.com>
2022-05-22 23:34:04 +0800

    修改

--- nacocpy/coc_dice.py
+++ nacocpy/coc_dice.py
@@ -1,44 +1,50 @@
 import random as rm
 
 
+class DiceTime:
+    """
+    This is a class record using time of dices
+
+    Args:
+        td4(int):   use time of D4 dice
+        td5(int):   use time of D5 dice
+        td6(int):   use time of D6 dice
+        td8(int):   use time of D8 dice
+        td10(int):  use time of D10 dice
+        td12(int):  use time of D12 dice
+        td20(int):  use time of D20 dice
+        td30(int):  use time of D30 dice
+        td100(int): use time of D100 dice
+    """
+
+    def __init__(self, td4=0, td5=0, td6=0, td8=0, td10=0, td12=0, td20=0, td30=0, td100=0):
+        self.td4 = td4
+        self.td5 = td5
+        self.td6 = td6
+        self.td8 = td8
+        self.td10 = td10
+        self.td12 = td12
+        self.td20 = td20
+        self.td30 = td30
+        self.td100 = td100
+
+
 class CocDice:
     """
+    coc_dice.CocDice()
+    此类包含了不同类型的骰子，并且能够监控每个骰子的使用次数
     This is a class contain different dices, and record using time of each dices
 
+    使用骰子之前传入QQid参数以便生成个人日志
     Before use this dice, pass your QQ id into the dice_user(str) to avoid one
     user have more than one dice logger.
 
     Args:
-        dice_user(str): The user of dice, to fit qq-bot api
-        dice_time(class DiceTime): The class of using time of dices
+        dice_user(str): 骰子用户，与qq-bot api 适应 / The user of dice, to fit qq-bot api
+        dice_time(class DiceTime): 使用次数类 / The class of using time of dices
     """
 
-    class DiceTime:
-        """
-        This is a class record using time of dices
-
-        Args:
-            td4(int):   use time of D4 dice
-            td5(int):   use time of D5 dice
-            td6(int):   use time of D6 dice
-            td8(int):   use time of D8 dice
-            td10(int):  use time of D10 dice
-            td12(int):  use time of D12 dice
-            td20(int):  use time of D20 dice
-            td30(int):  use time of D30 dice
-            td100(int): use time of D100 dice
-        """
-
-        def __init__(self, td4=0, td5=0, td6=0, td8=0, td10=0, td12=0, td20=0, td30=0, td100=0):
-            self.td4 = td4
-            self.td5 = td5
-            self.td6 = td6
-            self.td8 = td8
-            self.td10 = td10
-            self.td12 = td12
-            self.td20 = td20
-            self.td30 = td30
-            self.td100 = td100
+    dice_user = DiceTime()
 
     def __init__(self, dice_user, dice_time=DiceTime()):
         self.dice_time = dice_time
@@ -79,3 +85,19 @@ class CocDice:
     def d100(self):
         self.dice_time.td100 += 1
         return rm.randint(1, 100)
+
+
+def random_divide(divide_num, divide_part):
+    if type(divide_num) == int and type(divide_part) == int \
+            and divide_num >= 0 and divide_part >= 0 and divide_num <= divide_part:
+        return AttributeError
+    else:
+        divide_list = []
+        for i in range(divide_part):
+            divide_list += [1]
+            print(divide_list)
+        divide_num -= divide_part
+        for i in range(divide_part - 1):
+            rand = rm.randint(0, divide_part - 1)
+            divide_list[rand] += 1
+            print(divide_list)
--- nacocpy/coc_inv.py
+++ nacocpy/coc_inv.py
@@ -1,5 +1,7 @@
 import coc_dice as cdi
+import coc_error as cer
 import math
+import random as rd
 
 
 class Investigator:
@@ -31,6 +33,20 @@ class Investigator:
         self.difficult = difficult
         self.age = age
 
+    def __get__(self, instance, owner):
+        if not 15 <= type(self.age) <= 90:
+            return cer.CocAgeError()
+
+
+    @staticmethod
+    def edu_enhance(attr, dice):
+        if dice.d100() > attr:
+            attr += dice.d12()
+        if attr >= 99:
+            attr = 99
+        return attr
+
+
     def roll_attribute(self):
         # change with difficult
         if self.difficult == 0:
@@ -40,23 +56,23 @@ class Investigator:
         elif self.difficult == 2:
             d = 0.2
         # STR       3D6*5
-        self.Attr.STR = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5 * d)
+        self.Attr.STR = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5)
         # CON       3D6*5
-        self.Attr.CON = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5 * d)
+        self.Attr.CON = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5)
         # SIZ       2D6+6*5
-        self.Attr.SIZ = math.floor((self.dice.d6() + self.dice.d6() + 6) * 5 * d)
+        self.Attr.SIZ = math.floor((self.dice.d6() + self.dice.d6() + 6) * 5)
         # DEX       3D6*5
-        self.Attr.DEX = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5 * d)
+        self.Attr.DEX = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5)
         # APP       3D6*5
-        self.Attr.APP = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5 * d)
+        self.Attr.APP = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5)
         # INT       2D6+6*5
-        self.Attr.INT = math.floor((self.dice.d6() + self.dice.d6() + 6) * 5 * d)
+        self.Attr.INT = math.floor((self.dice.d6() + self.dice.d6() + 6) * 5)
         # POW       3D6*5
-        self.Attr.POW = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5 * d)
+        self.Attr.POW = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5)
         # EDU       2D6+6*5
-        self.Attr.EDU = math.floor((self.dice.d6() + self.dice.d6() + 6) * 5 * d)
+        self.Attr.EDU = math.floor((self.dice.d6() + self.dice.d6() + 6) * 5)
         # lucky     3D6*5
-        self.Attr.lucky = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5 * d)
+        self.Attr.lucky = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5)
         # DAM and PHY
         if self.Attr.SIZ + self.Attr.STR <= 64:
             self.Attr.DAM = -2
@@ -104,10 +120,51 @@ class Investigator:
         elif 89 > self.age:
             self.Attr.MOV -= 5
 
+        # age influence and enhance verification
+        if 15 <= self.age <= 19:
+            cdi_list = cdi.random_divide(2, 5)
+            self.Attr.SIZ -= cdi_list[0]
+            self.Attr.STR -= cdi_list[1]
+            self.Attr.EDU -= 5
+            lucky_2 = math.floor((self.dice.d6() + self.dice.d6() + self.dice.d6()) * 5)
+            lucky_1 = self.Attr.lucky
+            self.Attr.lucky = max(lucky_1, lucky_2)
+        elif self.age <= 29:
+            self.Attr.EDU = self.edu_enhance(self.Attr.EDU, self.dice)
+            self.Attr.APP -= 5
+        elif self.age <= 39:
+            for i in range(2):
+                self.Attr.EDU = self.edu_enhance(self.Attr.EDU, self.dice)
+            self.Attr.APP -= 10
+        elif self.age <= 49:
+            for i in range(3):
+                self.Attr.EDU = self.edu_enhance(self.Attr.EDU, self.dice)
+            self.Attr.APP -= 15
+        elif self.age <= 59:
+            for i in range(4):
+                self.Attr.EDU = self.edu_enhance(self.Attr.EDU, self.dice)
+            self.Attr.APP -= 20
+        elif self.age <= 69:
+            for i in range(4):
+                self.Attr.EDU = self.edu_enhance(self.Attr.EDU, self.dice)
+            self.Attr.APP -= 25
+        elif self.age <= 79:
+            for i in range(4):
+                self.Attr.EDU = self.edu_enhance(self.Attr.EDU, self.dice)
+            self.Attr.APP -= 30
+        elif self.age <= 89:
+            for i in range(4):
+                self.Attr.EDU = self.edu_enhance(self.Attr.EDU, self.dice)
+            self.Attr.APP -= 35
 
-inv = Investigator('10000000', 0, 28)
+
+
+
+
+
+
+inv = Investigator('10000000', 0, 100)
 inv.roll_attribute()
-
 print('STR', inv.Attr.STR)
 print('CON', inv.Attr.CON)
 print('SIZ', inv.Attr.SIZ)
